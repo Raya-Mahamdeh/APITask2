5c5ff78615727ce806a3bfe0ab3216d9681d6e83
Raya Mahamdeh <55127570+Raya-Mahamdeh@users.noreply.github.com>
2024-01-26 16:04:04 +0200

    Getting Data
    Get data from API using axios method

---
+++ assets/javascript.js
@@ -0,0 +1,32 @@
+
+async function getUser() {
+    try {
+      const response = await axios.get('https://dummyjson.com/products');
+      console.log(response.data.products);
+      const Products=response.data.products;
+      const result = Products.map(function(products){
+        return `
+        <div class="product">         
+        <h2>${products.title}</h2>
+        <p>Price: ${products.price}$</p>
+        <img src="${products.thumbnail}"/>
+        <button onclick="bringData(${products.id})">Read More</button>
+        </div> 
+        
+        ` 
+           
+    });
+    document.querySelector(".productSection").innerHTML = result;
+    } catch (error) {
+      console.error(error);
+    }
+    
+  }
+  function bringData(data){
+    localStorage.productID = data;
+    console.log(localStorage.productID);
+    window.location.href = "about.htm";
+
+}
+
+getUser();
